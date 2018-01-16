c1996ab786ae8e76ffb3370773c5d2d5c71e29fc
Phillip Maluta <phillip.maluta@volia-software.com>
2018-01-16 19:04:03 +0200

    Initial Commit

---
+++ TicTacToe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTacToe
+//
+//  Created by Philipp Maluta on 16.01.18.
+//  Copyright © 2018 com.ensoreus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
